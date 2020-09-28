5cced1d99a825759eecfd64618a103c2506bc35d
khoa.lva <khoa.le.insenco@gmail.com>
2020-09-28 11:23:32 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,11 +1,41 @@
+/*------check form start------*/
+(function() {
+'use strict';
+window.addEventListener('load', function() {
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+var forms = document.getElementsByClassName('needs-validation');
+// Loop over them and prevent submission
+var validation = Array.prototype.filter.call(forms, function(form) {
+form.addEventListener('submit', function(event) {
+if (form.checkValidity() === false) {
+event.preventDefault();
+event.stopPropagation();
+}
+form.classList.add('was-validated');
+}, false);
+});
+}, false);
+})();
+/*------check form end------*/
+
+/*------dropdown menu start------*/
+
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
+
+/*------dropdown menu start------*/
+
 /*------Swiper start------*/
 var swiper = new Swiper('.swiper-container-2', {
             centeredSlides: true,
             slidesPerView: 'auto',
             loop: true,
-            autoplay: {
-                delay: 5000,
-              },
+            // autoplay: {
+            //     delay: 5000,
+            //   },
             pagination: {
                 el: '.swiper-pagination',
             },
@@ -17,7 +47,3 @@ var swiper = new Swiper('.swiper-container-2', {
 
 /*------Swiper end------*/
 
-function toggleMenu() {
-  let menu = document.querySelector("#navbar-mobile");
-  menu.classList.toggle("d-none");
-}
